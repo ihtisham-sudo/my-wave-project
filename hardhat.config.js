b917b94c9bd83277e22d36a648a6a58973be1243
Ihtisham <ahsaanullah174@gmail.com>
2022-09-30 01:50:32 +0500

    Encrypted something fishy

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
+require("dotenv").config();
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     const accounts = await hre.ethers.getSigners();
 
@@ -17,11 +18,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: "0.8.17",
-    networks: {
-      goerli: {
-        url: "https://restless-magical-pond.ethereum-goerli.discover.quiknode.pro/0835360bb4dc22e1838cfb8bb1d9580f63c6e618/",
-        accounts: ["1165418f39a4f1523ea2c6684525d53660afee233830d516327870516db89f4c"]
-      },
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
